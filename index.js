103f0c93f9c0a3aff03329cc3f54e592604a65ac
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2023-07-22 00:26:24 +0600

    express setup done

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+// variables
+const port = process.env.PORT || 8000;
+
+const uri = process.env.MONGO_URI;
+
+// midddle ewares
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+// test api
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "welcome to dazzle martserver" });
+});
+
+// database
+mongoose
+  .connect(uri, {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
